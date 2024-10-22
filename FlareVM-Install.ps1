6429624bdc32e8fe93a3a40e44840949a11a2259
Majestic Logic <67641775+MajesticLogic@users.noreply.github.com>
2024-10-22 07:04:25 +0200

    Script to Install Flare VM
    pw flare
    Step 1: Unblock-File .\FlareVM-Install.ps1
    
    Step 2: Set-ExecutionPolicy Unrestricted -Force
    
    Step 3: .\FlareVM-Install.ps1

---
+++ FlareVM-Install.ps1
@@ -0,0 +1,19 @@
+﻿#Disable BITS and DoSvs service to Prevent Windows Updates
+
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\BITS" -Name "Start" -Value 4
+
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\DoSvc" -Name "Start" -Value 4
+
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\wuauserv" -Name "Start" -Value 4
+
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 1
+
+Set-MpPreference -DisableRealtimeMonitoring $true
+
+(New-Object net.webclient).DownloadFile('https://raw.githubusercontent.com/mandiant/flare-vm/main/install.ps1',"$([Environment]::GetFolderPath("Desktop"))\install.ps1")
+
+Unblock-File .\install.ps1
+
+Set-ExecutionPolicy Unrestricted -Force
+
+.\install.ps1 -customConfig "https://raw.githubusercontent.com/mandiant/flare-vm/main/config.xml" -password flare -noWait -noGui
